13cbeccbe074f7713c300b39e99bc92716d296cb
abmkc3 <abm9311@gmail.com>
2018-09-12 19:28:52 +0530

    hi

---
+++ title.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type ="text/javascript">
+var user = Number(prompt("Please enter your number", ""));
+if(user==1)
+{
+alert("your no is one);
+}
+</script>
+</head>
+<body>
+<form name="form1">
+</form>
+
+
+
+
+
+</body>
+</html>
